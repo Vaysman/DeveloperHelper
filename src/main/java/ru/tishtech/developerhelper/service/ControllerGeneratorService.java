b7343fdc48ffa889e4362f989e67789b82875fad
tish17 <tish17@mail.ru>
2020-04-15 14:34:52 +0300

    Added Logic For Capital And Small Letters

--- src/main/java/ru/tishtech/developerhelper/service/ControllerGeneratorService.java
+++ src/main/java/ru/tishtech/developerhelper/service/ControllerGeneratorService.java
@@ -4,13 +4,13 @@ import java.util.List;
 
 public class ControllerGeneratorService {
 
-    public static List<String> generateControllerData(List<String> data, String projectName, String groupId, String model) {
-
+    public static List<String> generateControllerData(List<String> data, String projectName, String groupId,
+                                                      String capitalModel, String smallModel) {
         for (int i = 0; i < data.size(); i++) {
             if (data.get(i).contains("{groupId}")) data.set(i, data.get(i).replace("{groupId}", groupId));
             if (data.get(i).contains("{projectName}")) data.set(i, data.get(i).replace("{projectName}", projectName));
-            if (data.get(i).contains("{Model}")) data.set(i, data.get(i).replace("{Model}", model));
-            if (data.get(i).contains("{model}")) data.set(i, data.get(i).replaceAll("\\{model}", model.toLowerCase()));
+            if (data.get(i).contains("{Model}")) data.set(i, data.get(i).replace("{Model}", capitalModel));
+            if (data.get(i).contains("{model}")) data.set(i, data.get(i).replaceAll("\\{model}", smallModel));
         }
         return data;
     }
