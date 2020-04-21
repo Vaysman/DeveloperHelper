24f406375f654a7f33f786ec2e352f346c95fc7e
tish17 <tish17@mail.ru>
2020-04-21 16:05:48 +0300

    Added Authorization

--- src/main/java/ru/tishtech/developerhelper/controller/MainController.java
+++ src/main/java/ru/tishtech/developerhelper/controller/MainController.java
@@ -2,15 +2,13 @@ package ru.tishtech.developerhelper.controller;
 
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
-import org.springframework.web.bind.annotation.GetMapping;
-import org.springframework.web.bind.annotation.ModelAttribute;
-import org.springframework.web.bind.annotation.PostMapping;
-import org.springframework.web.bind.annotation.RequestParam;
+import org.springframework.web.bind.annotation.*;
 import ru.tishtech.developerhelper.constants.VariableTypes;
 import ru.tishtech.developerhelper.model.Variable;
 import ru.tishtech.developerhelper.model.VariableStore;
 
 @Controller
+@RequestMapping("/generator")
 public class MainController {
 
     @ModelAttribute("variableTypes")
